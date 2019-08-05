c02eb7290cb1b906c68240f26efc1553124be658
Jack Ganbold <jack.ganbold@rhinosecuritylabs.com>
2019-08-05 15:59:51 -0700

    Update args names and print result in only script mode

--- modules/ecr__pull_repos/main.py
+++ modules/ecr__pull_repos/main.py
@@ -21,8 +21,8 @@ module_info = {
 DOCKER_LOGIN_SUCCEEDED = 'Login Succeeded'
 
 
-def get_aws_session(profile, region):
-    return boto3.session.Session(profile_name=profile, region_name=region)
+def get_aws_session(aws_cli_profile, aws_region):
+    return boto3.session.Session(profile_name=aws_cli_profile, region_name=aws_region)
 
 
 def docker_login(docker_client ,username, password, registry):
@@ -41,7 +41,7 @@ def docker_pull(docker_client, repo):
     return docker_pull_response
 
 def ecr_pull(args, data):
-    aws_session = get_aws_session(args['profile'], args['region'])
+    aws_session = get_aws_session(args['aws_cli_profile'], args['aws_region'])
     ecr_client = aws_session.client('ecr')
     token = ecr_client.get_authorization_token()
 
@@ -51,31 +51,29 @@ def ecr_pull(args, data):
 
     if DOCKER_LOGIN_SUCCEEDED == docker_login_response.get('Status'):
         count = 0
-        for tag in args['tags']:
-            repo_tag = args['repository_uri'] + ":" + tag
+        for tag in args['aws_ecr_repository_tags']:
+            repo_tag = args['aws_ecr_repository_uri'] + ":" + tag
             try:
                 docker_pull_response = docker_pull(docker_client, repo_tag)
                 out = 'Pulled {}'.format(docker_pull_response)
                 count += 1
-                data['payload']['tags'].append(tag)
+                data['payload']['aws_ecr_repository_tags'].append(tag)
                 print(out)
             except Exception as e:
                 print(e)
 
         data['count'] = count
-        data['payload']['repository_uri'] = args['repository_uri']
-        data['payload']['region'] = args['region']
-         
-        print('Result:')
-        print(json.dumps(data, indent=4, default=str))
+        data['payload']['aws_ecr_repository_uri'] = args['aws_ecr_repository_uri']
+        data['payload']['aws_region'] = args['aws_region'] 
+        
 
 def main(args):
     data  = {
         'count': 0,
         'payload': {
-            'region': None,
-            'repository_uri': None,
-            'tags': []
+            'aws_region': None,
+            'aws_ecr_repository_uri': None,
+            'aws_ecr_repository_tags': []
         }
     }
 
@@ -94,12 +92,12 @@ def summary(data):
 if __name__ == "__main__":
     print('Running module {}...'.format(module_info['name']))
     args = {
-        'profile':'cloudgoat',
-        'region':'us-east-1',
-        'repository_uri': '216825089941.dkr.ecr.us-east-1.amazonaws.com/pacu',
-        'tags': ['latest', 'hahah']
+        'aws_cli_profile':'cloudgoat',
+        'aws_region':'us-east-1',
+        'aws_ecr_repository_uri': '216825089941.dkr.ecr.us-east-1.amazonaws.com/pacu',
+        'aws_ecr_repository_tags': ['latest', 'hahah']
     }
-    
+
     data = main(args)
 
     if data is not None:
@@ -109,5 +107,8 @@ if __name__ == "__main__":
         if not isinstance(summary, str):
             raise TypeError(' The {} module\'s summary is {}-type instead of str. Make summary return a string.'.format(module_info['name'], type(summary)))
         
+        print('RESULT:')
+        print(json.dumps(data, indent=4, default=str))        
+
         print('{} completed.\n'.format(module_info['name']))
         print('MODULE SUMMARY:\n\n{}\n'.format(summary.strip('\n')))   
