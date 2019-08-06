d1230c97aaf7a73c9dc6888d09e1291ed662aa76
Jack Ganbold <jack.ganbold@rhinosecuritylabs.com>
2019-08-05 22:54:18 -0700

    Remove hardcoded aws regions

--- index.py
+++ index.py
@@ -198,13 +198,6 @@ def get_all_aws_regions():
     regions = aws_session.get_available_regions('ec2')
 
     return regions
-    # return [
-    #     'us-east-1','us-east-2','us-west-1','us-west-2','ap-east-1',
-    #     'ap-south-1','ap-northeast-3','ap-northeast-2','ap-southeast-1',
-    #     'ap-southeast-2','ap-northeast-1','ca-central-1','cn-north-1',
-    #     'cn-northwest-1','eu-central-1','eu-west-1','eu-west-2','eu-west-3',
-    #     'eu-north-1','me-south-1','sa-east-1','us-gov-east-1','us-gov-west-1'
-    # ]
 
 
 def main_menu():
